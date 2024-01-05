bfbcf4af7f6c00bbf33e28b3be46a26a4128e77e
domajzer <129312846+domajzer@users.noreply.github.com>
2024-01-05 21:42:01 +0100

    added automation.py
    UI automation ( testing first cases )

--- whirlpool-sparrow-client/automation.py
+++ whirlpool-sparrow-client/automation.py
@@ -1,21 +1,26 @@
-import pyautogui
+import subprocess
+import time
+
+def start_tmux_with_app(session_name, app_command):
+    subprocess.run(f"tmux new -d -s {session_name} {app_command}", shell=True)
+
+def send_keystroke_to_tmux(session_name, keystrokes):
+    for keystroke in keystrokes:
+        subprocess.run(f"tmux send-keys -t {session_name} {keystroke}", shell=True)
+        time.sleep(0.8)
 
 def main():
+    tmux_session_name = "sparrow_wallet"
+    sparrow_command = "/usr/src/app/Sparrow/bin/Sparrow --network testnet"
     
-    press_keys()
-
-def press_keys():
-    # Short delay to switch to the right window after running the script
-
-    # Pressing the keys "W", "P", "P"
-    pyautogui.press('w')
-    pyautogui.press('p')
-    pyautogui.press('p')
-
-# Call the function to execute the key presses
-press_keys()
-
-
+    start_tmux_with_app(tmux_session_name, sparrow_command)
+    time.sleep(10)
+    
+    start_mixing_keystrokes = ['W', 'Enter', 'Enter', 'Enter', 'Enter', 'P', 'Enter', 'U', 'Enter', 'Tab', 'Enter']
+    send_keystroke_to_tmux(tmux_session_name, start_mixing_keystrokes)
+    
+    stop_start_mixing_keystrokes = ['Tab', 'Enter', 'Tab', 'Tab', 'Enter']
+    send_keystroke_to_tmux(tmux_session_name, stop_start_mixing_keystrokes)
 
 if __name__ == '__main__':
     main()
