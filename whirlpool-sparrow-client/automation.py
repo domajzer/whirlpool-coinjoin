3aa30cb244c26dcef41f89dfdffb1a050f1a81fa
domajzer <129312846+domajzer@users.noreply.github.com>
2024-01-21 22:04:54 +0100

    Update automation.py
    added creation of wallet and getting its address. 
    QoL updates
    Bettered up the code base by adding utility.py

--- whirlpool-sparrow-client/automation.py
+++ whirlpool-sparrow-client/automation.py
@@ -2,14 +2,16 @@ import subprocess
 import time
 import re
 import argparse
-import os
+import utility
 
 class Options:
-    def __init__(self, debug, debugf, pool, mix):
+    def __init__(self, debug, debugf, pool, mix, create, name):
         self.debug = debug
         self.debugf = debugf
         self.pool = pool
         self.mix = mix
+        self.create = create
+        self.name = name
         
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Automation Script')
@@ -18,44 +20,11 @@ def parse_arguments():
     parser.add_argument('-debugf', '--debugf', action='store_true', help='Enable full debug option')
     parser.add_argument('-pool', '--pool', action='store_true', help='Enable pool option')
     parser.add_argument('-mix', '--mix', action='store_true', help='Enable mix option')
+    parser.add_argument('-create', '--create', action='store_true', help='Set the name')
+    parser.add_argument('-name', '--name', action='store', type=str, help='Set the name')
     
     args = parser.parse_args()
-    return Options(debug=args.debug, debugf=args.debugf, pool=args.pool, mix=args.mix)
-
-def get_ip_address():
-    try:
-        result = subprocess.check_output("ip addr", shell=True).decode()
-        
-        ip_addresses = re.findall(r'inet (\d+\.\d+\.\d+\.\d+)', result)
-
-        return ip_addresses 
-    except subprocess.CalledProcessError as e:
-        return f"An error occurred: {e}"
-
-def system_info():
-    system_var = os.uname()
-    print(*system_var)
-    print(get_ip_address())
-
-def capture_tmux_output(session_name, pane_id, output_file):
-    cmd = f"tmux capture-pane -p -t {session_name}:{pane_id}"
-    output = subprocess.check_output(cmd, shell=True).decode('utf-8')
-    
-    with open(output_file, "w") as file:
-        file.write(output)
-
-def start_tmux_with_app(session_name, app_command):
-    subprocess.run(f"tmux new -d -s {session_name} {app_command}", shell=True)
-
-def send_keystroke_to_tmux(session_name, keystrokes):
-    for keystroke in keystrokes:
-        subprocess.run(f"tmux send-keys -t {session_name} {keystroke}", shell=True)
-        time.sleep(2.5)
-
-def print_tmux_screen(file_path):
-    with open(file_path, 'r') as file:
-        contents = file.read()
-        print(contents)
+    return Options(debug=args.debug, debugf=args.debugf, pool=args.pool, mix=args.mix, create=args.create, name=args.name)
 
 def check_wallet_initialization(file_path):
     try:
@@ -77,18 +46,18 @@ def check_wallet_initialization(file_path):
 
 def initialize_wallet(tmux_session_name, options):
     start_mixing_keystrokes = ['W', 'Enter', 'Enter', 'Enter', 'Enter', 'Tab', 'Enter']
-    send_keystroke_to_tmux(tmux_session_name, start_mixing_keystrokes)
+    utility.send_keystroke_to_tmux(tmux_session_name, start_mixing_keystrokes, options)
     time.sleep(2)
     
-    capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
     if options.debug: 
-        print_tmux_screen('output.txt')
+        utility.print_tmux_screen('output.txt')
     
     check = check_wallet_initialization('output.txt')
     if check == 1:
-        start_mixing_keystrokes = ['P', 'Enter', 'S', 'Enter', 'Tab', 'Enter', 'Tab', 'Enter', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Enter'] #TODO
-        send_keystroke_to_tmux(tmux_session_name, start_mixing_keystrokes)
-        time.sleep(1.5)
+        start_mixing_keystrokes = ['P', 'Enter', 'S', 'Enter', 'Tab', 'Enter', 'Tab', 'Enter', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'Enter', 'Tab', 'Enter', 'Tab', 'Enter'] #TODO
+        utility.send_keystroke_to_tmux(tmux_session_name, start_mixing_keystrokes, options)
+        time.sleep(10)
 
 def add_to_pool(tmux_session_name, options, file_path):
     #start_mixing_keystrokes = ['W', 'Enter', 'Enter', 'Enter', 'U', 'Enter', 'Enter', 'Tab', 'Enter', 'Tab', 'Tab', 'Tab', 'Enter', 'Tab', 'Enter', 'Enter', 'Tab', 'Enter', 'Tab', 'Tab', 'Enter']
@@ -97,10 +66,10 @@ def add_to_pool(tmux_session_name, options, file_path):
     back_keystrokes = ['Tab', 'Enter', 'Tab', 'Tab', 'Enter']
     date_pattern = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}"
     
-    send_keystroke_to_tmux(tmux_session_name, start_mixing_keystrokes)
-    capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+    utility.send_keystroke_to_tmux(tmux_session_name, start_mixing_keystrokes, options)
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
     if options.debug: 
-        print_tmux_screen('output.txt')
+        utility.print_tmux_screen('output.txt')
     time.sleep(1.5)
     
     try:
@@ -112,9 +81,9 @@ def add_to_pool(tmux_session_name, options, file_path):
                 
             elif "Undefined" in content:
                 for _ in range(25):
-                    capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+                    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
                     if options.debug: 
-                        print_tmux_screen('output.txt')
+                        utility.print_tmux_screen('output.txt')
                         
                     with open(file_path, 'r') as file:
                         content = file.read()
@@ -138,29 +107,29 @@ def add_to_pool(tmux_session_name, options, file_path):
     start_mixing_keystrokes = ['Enter', 'Tab', 'Enter', 'Tab', 'Tab', 'Tab', 'Enter', 'Tab', 'Enter', 'Enter', 'Tab', 'Enter', 'Tab', 'Enter']
     final_keystrokes = back_keystrokes if is_defined else start_mixing_keystrokes
     
-    send_keystroke_to_tmux(tmux_session_name, final_keystrokes)
-    capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+    utility.send_keystroke_to_tmux(tmux_session_name, final_keystrokes, options)
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
     if options.debug: 
-        print_tmux_screen('output.txt')
+        utility.print_tmux_screen('output.txt')
     
 def start_mix(tmux_session_name, options):
     counter = 0
     #start premix 
     start_mixing_keystrokes = ['W', 'Enter', 'Enter', 'P', 'Enter', 'U', 'Enter', 'Tab', 'Enter']
-    send_keystroke_to_tmux(tmux_session_name, start_mixing_keystrokes)
-    capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+    utility.send_keystroke_to_tmux(tmux_session_name, start_mixing_keystrokes, options)
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
     
     if options.debug: 
-        print_tmux_screen('output.txt')
+        utility.print_tmux_screen('output.txt')
     
     try:
         with open('output.txt', 'r') as file:
             content = file.read()
             if "Stop Mixing" not in content:
                 for _ in range(20):
-                    capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+                    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
                     if options.debug: 
-                        print_tmux_screen('output.txt')
+                        utility.print_tmux_screen('output.txt')
                         
                     with open('output.txt', 'r') as file:
                         content = file.read()
@@ -182,10 +151,10 @@ def start_mix(tmux_session_name, options):
                             
     #return to start after premix
     stop_start_mixing_keystrokes = ['Tab', 'Tab', 'Tab', 'Enter', 'Tab', 'Tab', 'Enter']
-    send_keystroke_to_tmux(tmux_session_name, stop_start_mixing_keystrokes)
-    capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+    utility.send_keystroke_to_tmux(tmux_session_name, stop_start_mixing_keystrokes, options)
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
     if options.debug: 
-        print_tmux_screen('output.txt')
+        utility.print_tmux_screen('output.txt')
     
     try:
         with open('output.txt', 'r') as file:
@@ -203,17 +172,81 @@ def start_mix(tmux_session_name, options):
     except Exception as e:
         print(f"An error occurred: {e}")
     
+def create_wallet(tmux_session_name, options):
+    seed_file = 'seed.txt'
+    start_keystrokes = ['W', 'Enter', 'C', 'Enter']
+    utility.send_keystroke_to_tmux(tmux_session_name, start_keystrokes, options)
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+    
+    if options.debug: 
+        utility.print_tmux_screen('output.txt')
+    
+    continue_keystrokes = list(options.name)
+    utility.send_keystroke_to_tmux(tmux_session_name, continue_keystrokes, options)
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+    
+    if options.debug: 
+        utility.print_tmux_screen('output.txt')
+    
+    continue_keystrokes_2 = ['Tab', 'Enter', 'Enter', 'Tab', 'Tab', 'Tab', 'Enter', 'Enter', '1', '1', '1', 'Enter', 'Tab', 'Tab', 'Enter']
+    utility.send_keystroke_to_tmux(tmux_session_name, continue_keystrokes_2, options)
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+    
+    parsed_seed_words = utility.parse_seed('output.txt',seed_file)
+    print(parsed_seed_words)    
+    if options.debug: 
+        utility.print_tmux_screen('output.txt')
+    
+    continue_keystrokes_3 = ['Tab', 'Tab', 'Enter', 'Tab', 'Enter', 'Tab', 'Enter']
+    utility.send_keystroke_to_tmux(tmux_session_name, continue_keystrokes_3, options)
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+
+    if options.debug: 
+        utility.print_tmux_screen('output.txt')
+
+    try:
+        with open('output.txt', 'r') as file:
+            content = file.read()
+
+            if ("Wallets" in content) and ("Preferences" in content) and ("Quit" in content) and ("Connecting" not in content):
+                print(f"\033[32mWallet creation successful. Seed can be found {seed_file}.\033[0m")
+                return 0
+            else:
+                print("\033[31mWallet creation failed\033[0m")
+                return 1
+                
+    except FileNotFoundError:
+        print(f"File not found: {'output.txt'}")
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+def get_adress(tmux_session_name, options):
+    start_keystrokes = ['W', 'Enter', 'Enter', 'R', 'Enter']
+    utility.send_keystroke_to_tmux(tmux_session_name, start_keystrokes, options)
+    utility.capture_tmux_output('sparrow_wallet', '0', 'output.txt')
+    
+    address = utility.parse_address("output.txt")
+    print(F"$$Wallet$$Address$$: {address}")
+    
+    return_keystrokes = ['Tab', 'Enter', 'Tab', 'Enter']
+    utility.send_keystroke_to_tmux(tmux_session_name, return_keystrokes, options)
+
 def main():
     options = parse_arguments()
     tmux_session_name = "sparrow_wallet"
     sparrow_command = "/usr/src/app/Sparrow/bin/Sparrow --network testnet"
     
-    start_tmux_with_app(tmux_session_name, sparrow_command)
-    time.sleep(7)
-    system_info()
+    utility.start_tmux_with_app(tmux_session_name, sparrow_command)
+    time.sleep(5)
+    utility.system_info(options)
     
     #initialize the wallet
-    initialize_wallet(tmux_session_name, options)
+    if options.create:
+        create_wallet(tmux_session_name, options)
+        get_adress(tmux_session_name, options)
+    else:
+        initialize_wallet(tmux_session_name, options)
     #add to pool
     if options.pool:
         add_to_pool(tmux_session_name, options, 'output.txt')
@@ -225,7 +258,7 @@ def main():
     #start_mix(tmux_session_name, options)
     
     while(1):
-        time.sleep(1)
+       time.sleep(1)
 
 if __name__ == '__main__':
     main()
