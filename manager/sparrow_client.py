f05b096651970fbff02194c63e617d81e364fb98
domajzer <domajzer@gmail.com>
2024-03-31 00:29:01 +0100

    fixes for a shared list in SparrowClient class

--- manager/sparrow_client.py
+++ manager/sparrow_client.py
@@ -1,14 +1,14 @@
-
 class SparrowClient:
     def __init__(
-        self, host="localhost", port=37128, name="whirlpool-sparrw-client", delay=0, proxy="", amount=None, address=[], mnemonic=None, account_number=0, connected=False, mixed=False
+        self, host="localhost", port=37128, name="whirlpool-sparrw-client", delay=0, proxy="", amount=None, address=None, mnemonic=None, account_number=0, connected=False, mixed=False
         ):
         self.host = host
         self.port = port
         self.name = name
         self.delay = delay
+        self.proxy = proxy
         self.amount = amount
-        self.address = address
+        self.address = address if address is not None else []
         self.mnemonic = mnemonic
         self.account_number = account_number
         self.connected = connected
